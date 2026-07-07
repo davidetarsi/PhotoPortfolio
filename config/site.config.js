038e4febcffa25fd13f3765864741b2f6c5bb1d0
Davide Tarsi <davidetarsi.dev@gmail.com>
2026-07-07 20:20:18 +0200

    fix: use lh3.googleusercontent.com/d/ for hero and cover direct image URLs

--- config/site.config.js
+++ config/site.config.js
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: 'Nome Fotografo',
   bio: 'Una breve descrizione del fotografo.',
   language: 'it',
-  heroImageUrl: 'https://drive.google.com/file/d/1E6EnyGyFXP3EH2cOsSYVwPSzgJ6UzWn-',
+  heroImageUrl: 'https://lh3.googleusercontent.com/d/1E6EnyGyFXP3EH2cOsSYVwPSzgJ6UzWn-',
   social: {
     // instagram: 'https://instagram.com/...',
   },
