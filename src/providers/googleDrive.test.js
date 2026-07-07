c687baf73636ae768ef320969318dacbe29c5e34
Davide Tarsi <davidetarsi.dev@gmail.com>
2026-07-07 22:09:18 +0200

    perf: grid s400→s500, lightbox s900→s1300

--- src/providers/googleDrive.test.js
+++ src/providers/googleDrive.test.js
@@ -33,8 +33,8 @@ describe('listPhotos', () => {
     expect(photos).toHaveLength(2)
     expect(photos[0]).toEqual({
       name: '01_alba.jpg',
-      gridUrl: 'https://lh3.google.com/abc=s400',
-      fullUrl: 'https://lh3.google.com/abc=s900',
+      gridUrl: 'https://lh3.google.com/abc=s500',
+      fullUrl: 'https://lh3.google.com/abc=s1300',
     })
     expect(photos[1].name).toBe('02_tramonto.jpg')
   })
