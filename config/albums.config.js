1a476a927619d574d22fb5b4aeb752716b65eec3
Davide Tarsi <davidetarsi.dev@gmail.com>
2026-07-10 18:04:51 +0200

    add album

--- config/albums.config.js
+++ config/albums.config.js
@@ -5,4 +5,10 @@ export const albums = [
     description: 'Foto sport',
     coverUrl: 'https://pub-f795b3dcc64b49348b6805cd460aa1e7.r2.dev/sport/4x5-crop-IMG_8689-.webp',  // dopo l'upload: 'https://pub-xxx.r2.dev/sport-album/nome-copertina.webp'
   },
+  {
+    slug: 'around-the-world',
+    title: 'Around the world',
+    description: 'Foto viaggio',
+    coverUrl: 'https://pub-f795b3dcc64b49348b6805cd460aa1e7.r2.dev/around-the-world/4x5-2637.webp',  // dopo l'upload: 'https://pub-xxx.r2.dev/sport-album/nome-copertina.webp'
+  },
 ];
