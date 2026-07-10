8567e11861128ecfe51177ed6c08bf858ae45a29
Davide Tarsi <davidetarsi.dev@gmail.com>
2026-07-10 12:23:06 +0200

    fix(test): update stale Nav test name to /contatti

--- src/components/Nav.test.js
+++ src/components/Nav.test.js
@@ -21,7 +21,7 @@ describe('renderNav', () => {
     expect(links[0].textContent).toBe('Scrivimi');
   });
 
-  it('contatti link points to /contatti.html', () => {
+  it('contatti link points to /contatti', () => {
     renderNav(container, { name: 'F' }, { nav: { contattiLabel: 'C' } });
     expect(container.querySelector('.site-nav__links a').getAttribute('href')).toBe('/contatti');
   });
